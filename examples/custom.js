c9f3354cf652e99b1e0700f6d28a2d482fb39fbc
Matthias Mohr <webmaster@mamo-net.de>
2024-03-18 14:00:15 +0100

    Add an example where people can provide their own STAC for demo purposes

---
+++ examples/custom.js
@@ -0,0 +1,62 @@
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import STAC from '../src/ol/layer/STAC.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+import View from 'ol/View.js';
+import proj4 from 'proj4';
+import {register} from 'ol/proj/proj4.js';
+
+register(proj4); // required to support source reprojection
+
+const background = new TileLayer({
+  source: new OSM(),
+});
+
+const map = new Map({
+  target: 'map',
+  layers: [background],
+  view: new View({
+    center: [0, 0],
+    zoom: 0,
+  }),
+});
+
+window.onload = function () {
+  document.getElementById('load-btn').addEventListener('click', showUrl);
+  document.getElementById('reset-btn').addEventListener('click', resetUrl);
+  showUrl();
+};
+
+let layer;
+
+function resetUrl() {
+  document.getElementById('url-input').value = '';
+  showUrl();
+}
+
+function showUrl() {
+  if (layer) {
+    map.removeLayer(layer);
+  }
+
+  const url = document.getElementById('url-input').value;
+  if (!url) {
+    return;
+  }
+
+  try {
+    layer = new STAC({
+      displayWebMapLink: true,
+      url,
+    });
+
+    layer.on('sourceready', () => {
+      const view = map.getView();
+      view.fit(layer.getExtent());
+    });
+
+    map.addLayer(layer);
+  } catch (error) {
+    alert(error.message);
+  }
+}
